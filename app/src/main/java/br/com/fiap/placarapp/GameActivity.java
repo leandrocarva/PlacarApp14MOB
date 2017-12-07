8231da7cd37543812e5a4b8da0dd20d7676ffafe
logonrm <logonrm@fiap.com.br>
2017-12-06 22:04:55 -0200

    Criacao da tela placar

--- app/src/main/java/br/com/fiap/placarapp/GameActivity.java
+++ app/src/main/java/br/com/fiap/placarapp/GameActivity.java
@@ -1,8 +1,10 @@
 package br.com.fiap.placarapp;
 
 import android.content.Intent;
+import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class GameActivity extends AppCompatActivity {
@@ -13,13 +15,22 @@ public class GameActivity extends AppCompatActivity {
     private TextView tvTimeCasa;
     private TextView tvTimeVisitante;
 
+    private TextView tvPlacarCasa;
+    private TextView tvPlacarVisitante;
+
+    private int golCasa;
+    private int golVisitante;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
-        tvTimeCasa = (TextView)findViewById(R.id.tvTimeCasa);
-        tvTimeVisitante = (TextView)findViewById(R.id.tvTimeVisitante);
+        tvTimeCasa = (TextView) findViewById(R.id.tvTimeCasa);
+        tvTimeVisitante = (TextView) findViewById(R.id.tvTimeVisitante);
+
+        tvPlacarCasa = (TextView) findViewById(R.id.tvPlacarCasa);
+        tvPlacarVisitante = (TextView) findViewById(R.id.tvPlacarVisitante);
 
         if (getIntent() != null) {
             timeCasa = getIntent().getStringExtra("TIMECASA");
@@ -28,5 +39,33 @@ public class GameActivity extends AppCompatActivity {
             tvTimeCasa.setText(timeCasa);
             tvTimeVisitante.setText(timeVisitante);
         }
+         /* quando a tela é recriada inserimos os dados*/
+        if (savedInstanceState != null) {
+            golCasa = savedInstanceState.getInt("GOLCASA");
+            golVisitante = savedInstanceState.getInt("GOLVISITANTE");
+        }
+
+        tvPlacarCasa.setText(String.valueOf(golCasa));
+        tvPlacarVisitante.setText(String.valueOf(golVisitante));
+    }
+
+
+
+    public void golCasa(View v) {
+        golCasa++;
+        tvPlacarCasa.setText(String.valueOf(golCasa));
+    }
+
+    public void golVisitante(View v) {
+        golVisitante++;
+        tvPlacarVisitante.setText(String.valueOf(golVisitante));
+    }
+
+    @Override /* para quando girar o cel ou entrar uma ligação faz com que não perdemos os dados*/
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        outState.putInt("GOLCASA",golCasa);
+        outState.putInt("GOLVISITANTE",golVisitante);
     }
 }
